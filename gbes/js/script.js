befbec30a971c6bd3be56b0f795d18baed5bb56a
Viordik <fataliev2009@rambler.ru>
2018-05-31 11:42:11 +0300

    Сделаны все страницы сайта, внесены изминения и правки

--- gbes/js/script.js
+++ gbes/js/script.js
@@ -7,9 +7,42 @@ btnMenu.addEventListener('click', function () {
 });
 
 //===============================================
-//Раскрывающиеся блоки с историей компании на странице О компании
-var btnYear = document.querySelectorAll('.history-company__btn-year');
+//Раскрывающиеся блоки с историей компании на странице "О компании"
+var yearButtons = document.querySelectorAll('.history-company__btn-year');
 var blockHide = document.querySelectorAll('.history-company__text-hide');
+var lotHouse = document.querySelectorAll('.lot-house__description-hide');
+var news = document.querySelectorAll('.text-hide')
+
+console.log(lotHouse);
+
+Array.prototype.forEach.call(yearButtons, function(yearButton, index) {
+  yearButton.addEventListener('click', function(evt) {
+    blockHide[index].classList.toggle('history-company__text-hide--active');
+    // lotHouse[index].classList.toggle('lot-house__description-hide--active');
+    // news[index].classList.toggle('text-hide--active');
+  })
+});
+
+//===============================================
+
+// $(document).ready(function(){
+//   $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//           item:1
+//         },
+//         600:{
+//           item:3
+//         },
+//         1000:{
+//           item:5
+//         }
+//     }
+//   })
+// });
 
 
 //===============================================
